f3798bd5470f5992358b8904a336c9e174006597
Houki Kohmoli <32835505+kohmoli@users.noreply.github.com>
2023-06-06 03:35:35 +0900

    Update ch04_3.sh

--- ch04_3.sh
+++ ch04_3.sh
@@ -3,4 +3,4 @@ docker image build -t ch04/todoapi:latest .
 docker image tag ch04/todoapi:latest localhost:5000/ch04/todoapi:latest
 docker image push localhost:5000/ch04/todoapi:latest
 docker container exec -it manager docker stack deploy -c /stack/todo-app.yml todo_app
-docker container exec -it manager docker service logs -f todo_app_api
+#docker container exec -it manager docker service logs -f todo_app_api
