0e6b12749d3cb4221f7822d3252d2a5a1e7ec817
Houki Kohmoli <32835505+kohmoli@users.noreply.github.com>
2023-06-06 00:17:33 +0900

    Add files via upload

---
+++ ch03_5.sh
@@ -0,0 +1,18 @@
+cd ch03_5
+
+# 3つのDockerホストを立ち上げる（manager, worker01, worker02）
+docker-compose up
+
+# managerをSwarm managerとしてSwarmクラスタを作成する
+docker container exec -it manager docker swarm init
+
+# join-tokenを取得する
+JOIN_TOKEN=$(docker swarm join-token -q worker)
+
+# worker01, worker02，worker03をSwarmクラスタに登録する
+docker container exec -it worker01 docker swarm join --token $JOIN_TOKEN manager:2377
+docker container exec -it worker02 docker swarm join --token $JOIN_TOKEN manager:2377
+docker container exec -it worker03 docker swarm join --token $JOIN_TOKEN manager:2377
+
+# 複数のホストをOverlayネットワークで通信できるようにする
+docker container exec -it manager docker network create --driver=overlay --attachable todoapp
