75ad6585fab146949d9245d14061c048198031de
Houki Kohmoli <32835505+kohmoli@users.noreply.github.com>
2023-06-06 00:28:47 +0900

    Update ch03_5.sh

--- ch03_5.sh
+++ ch03_5.sh
@@ -1,13 +1,13 @@
 cd ch03_5
 
 # 3つのDockerホストを立ち上げる（manager, worker01, worker02）
-docker-compose up
+docker-compose up -d
 
 # managerをSwarm managerとしてSwarmクラスタを作成する
 docker container exec -it manager docker swarm init
 
 # join-tokenを取得する
-JOIN_TOKEN=$(docker swarm join-token -q worker)
+JOIN_TOKEN=$(docker container exec -it manager docker swarm join-token -q worker)
 
 # worker01, worker02，worker03をSwarmクラスタに登録する
 docker container exec -it worker01 docker swarm join --token $JOIN_TOKEN manager:2377
