6a8097e889984588ee1d7a19ba23c5c2a14d33df
Houki Kohmoli <32835505+kohmoli@users.noreply.github.com>
2023-06-06 09:11:56 +0900

    Update ch03_5.sh

--- ch03_5.sh
+++ ch03_5.sh
@@ -18,21 +18,3 @@ docker container exec -it worker03 docker swarm join --token $JOIN_TOKEN manager
 
 # 複数のホストをOverlayネットワークで通信できるようにする
 docker container exec -it manager docker network create --driver=overlay --attachable todoapp
-
-# Dockerfileのビルド
-cd tododb
-docker image build -t ch04/tododb:latest .
-
-# registryコンテナにDocker imageをpushする
-docker image tag ch04/tododb:latest localhost:5000/ch04/tododb:latest
-docker image push localhost:5000/ch04/tododb:latest
-
-# StackをSwarmクラスタにデプロイ
-# ch03_5_1のdocker-compose.ymlをstackの下へ移動させておく
-docker container exec -it manager docker stack deploy -c /stack/todo-mysql.yml todo_mysql
-
-# Serviceがデプロイされているか確認
-docker container exec -it manager docker service ls
-
-# MasterコンテナがSwarm上のどのノードに配置されているのか確認する
-docker container exec -it manager docker service ps todo_mysql_master --no-trunc --filter "desired-state=running"
